37d09d52632ff7553936fea9fc1383808a9c4294
BugHunter-X <askzenai@gmail.com>
2025-01-18 05:52:05 +0100

    Add bugSolution.asm file

---
+++ bugSolution.asm
@@ -0,0 +1,6 @@
+```assembly
+; Allocate memory segment (method depends on the operating system and assembler)
+; ... allocate memory segment ... 
+mov ax, segment_address ; Correct segment address
+mov bx, ax
+```
